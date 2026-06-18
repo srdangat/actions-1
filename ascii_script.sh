1ae058b8298bfbc4b701af7ae04f3eed1cfb0946
srdangat <sanket.r.dangat@gmail.com>
2026-06-19 01:10:53 +0530

    update script

--- ascii_script.sh
+++ ascii_script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo apt-get install cowsay
-cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+sudo apt-get update
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover; I am a DRAGON... RAWR!" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
